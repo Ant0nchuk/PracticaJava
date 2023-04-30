a1dcbc3ef63612a1b03661785e20c1d4e3b315f3
Ant0nchuk <alejandro.antonchuk.99@gmail.com>
2023-04-30 20:58:30 -0300

    Adicion del archivo Practica.java y la carpeta Imperativa

---
+++ Imperativa/OperadorTernario.java
@@ -0,0 +1,18 @@
+package Imperativa;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        
+        //testeando el operador ternario
+
+        boolean esUnAuto = false;
+
+        boolean fueUnAuto = (esUnAuto)?true:false;
+
+        System.out.println(fueUnAuto);
+
+
+        
+       }
+    
+}
---
+++ Imperativa/PracticaImperativa.java
@@ -0,0 +1,23 @@
+package Imperativa;
+
+public class PracticaImperativa {
+    
+    public static void main(String[] args) {
+        
+        //declaracion de variables
+        int variableNumerica = 10;
+
+        //declaracion multiple
+
+        int var1,var2,var3 = 0;
+
+        int result = 2 + 3 * 6 / 3 - 1;
+
+        System.out.println("El resultado es: "+result);
+
+        /*
+         *  // comentario lineal
+         * comentarios multilineal
+         */
+    }
+}
