34d0ef192930debbdb65382d780ab68a0632ed85
Ant0nchuk <107375102+Ant0nchuk@users.noreply.github.com>
2023-09-22 22:29:20 -0300

    Ejercitacion hasta el ej 6

---
+++ Java2023/ProgramacionImperativa/Clase02.java
@@ -0,0 +1,71 @@
+package ProgramacionImperativa;
+
+public class Clase02 {  
+
+    public static void main(String[] args) { 
+        
+        /* Ejercicio numero 1 NIVEL INICIADO JAVA JEDI
+
+        int num1 = 10;
+        int num2 = 24;
+        int num3 = num1 + num2;
+        System.out.println("El resultado es: " + num3);
+        */
+        
+        /* Ejercicio numero 2 
+
+        int num1 = 10;
+        int num2 = 24;
+        int num3 = num1 + num2;
+        String paridad = (num3%2 == 0)? "par" : "impar";
+        System.out.println("El resultado es " + num3 + " y es " + paridad);
+        */
+
+        /*Ejercicio numero 3  */ 
+        /* 
+        char car1 = 'A';
+        char car2 = 'L';
+        char car3 = 'E';
+        char car4 = 'J';
+        char car5 = 'A';
+        char car6 = 'N';
+        char car7 = 'D';
+        char car8 = 'R';
+        char car9 = 'O';
+        
+        System.out.println("Mi nombre es: " + car1 + car2 +
+        car3 + car4 + car5 + car6 + car7 + car8 + car9);
+
+        
+        */
+/* Ejercicio numero 4
+
+        int num1 = 80;
+        int num2 = 20;
+        int resultado = num1 + num2;
+        String evaluacion = (num1 % num2 == 0)? "son divisibles" : "No son divisibles";
+
+        System.out.println("Los numeros " + num1 + " y " + num2 + " "+  evaluacion + 
+        " y el resultado es: " + resultado );
+
+        */ 
+        //ejercicio numero 5
+        //System.out.println("  *\n ***\n*****");
+
+        //ejercicio numero 6
+        /* 
+        String nombre = "Bobi";
+        String pel = "El Justiciero";
+        System.out.println("Nombre de la proxima bebida: " + nombre + " " + pel);
+        */
+        
+
+
+
+
+
+
+    }
+    
+
+}
