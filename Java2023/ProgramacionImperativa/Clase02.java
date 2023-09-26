87fb3f60c79f83e51f8587763f9c393b10d53370
Ant0nchuk <107375102+Ant0nchuk@users.noreply.github.com>
2023-09-26 15:33:18 -0300

    hasta ejercicios caballero java jedi

--- Java2023/ProgramacionImperativa/Clase02.java
+++ Java2023/ProgramacionImperativa/Clase02.java
@@ -1,4 +1,5 @@
 package ProgramacionImperativa;
+import java.util.Scanner;
 
 public class Clase02 {  
 
@@ -59,7 +60,73 @@ public class Clase02 {
         System.out.println("Nombre de la proxima bebida: " + nombre + " " + pel);
         */
         
+        // Ejercicio numero 7
 
+        //double num1 = 2309.00;
+        //double num2 = 082.078;
+        //int num3 = 23;
+        //int num4 = 87; 
+        //double op1 = num1 * num2;
+        //int op2 = num3 * num4;
+        //double op3 = op1 + op2;
+
+        // Ejercicio numero 8
+
+        /* 
+        String nom = "Alejandro Antonchuk";
+        String edad = "24";
+        System.out.println("Mi nombre es " + nom + "\nMi edad es " + edad);
+        */
+
+        // Ejercicio numero 9
+            /*
+        int num1 = 3;
+        int pot2 = num1 * num1;
+        int pot3 = num1 * num1 * num1;
+        System.out.println("Potencia 2: " + pot2 + "\n Potencia 3: " + pot3);
+
+        */
+
+        //-----------------------------------------------------------------------
+        //Ejercicio numero 1 > NIVEL PADAWAN JAVA JEDI
+
+        //int num1 = 0;
+        //num1++; // asi 20 veces jajajaj
+
+        //num1--; //asi 20 veces para el ej numero 2
+
+
+        //Ejercicio numero 4
+        /*
+        int edad1 = 21;
+        int edad2 = 24;
+        int edad3 = 20;
+        String nom1 = "Julian";
+        String nom2 = "Alejandro";
+        String nom3 = "Lucas";
+        int prom = (edad1 + edad2 + edad3) / 3;
+        System.out.println("Nombre 1: "+ nom1 + " y Edad 1: "+ edad1 +
+        "\n Nombre2: "+ nom2 + " y edad2: "+ edad2 + 
+        "\n Nombre3: "+ nom3 + " y edad3: "+ edad3 + 
+        "\n El promedio de edades es: "+ prom);
+        */
+
+        //Ejercicio numero 5
+
+        //int num1 = 74;
+        //double CONST1 = num1 /  2.205; 
+        //System.out.println("El resultado es: "+ CONST1);
+
+        //EJERCICIOS NIVEL CABALLERO JAVA JEDI
+        //EJERCICIO NUMERO 1
+        /*
+        double num1 = 23.08498;
+        int num2 = 10;
+        double resultado = (num1 * num2) / 100;
+        System.out.println("El porcentaje es: "+ resultado +"%");
+        */
+
+        //ejercicio numero 2
 
 
 
